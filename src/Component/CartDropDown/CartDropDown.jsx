0f3929421732f52563b62716151623c25f074a95
Park cloud <340688748@qq.com>
2021-02-22 14:46:25 +0800

    create cartItem,CartDropDown component with the ability to read redux store vaule

--- src/Component/CartDropDown/CartDropDown.jsx
+++ src/Component/CartDropDown/CartDropDown.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { CartItem } from '../CartItem/CartItem';
+import { useSelector } from 'react-redux';
 import { CustomButton } from '../SignIn-and-SignUp-Page/CustomButton';
 import '../CartDropDown/CartDropDown.styles.scss';
 export const CartDropDown = () => {
+	const cartItems = useSelector((state) => state.cart.cartItems);
 	return (
 		<div className='cart-dropdown'>
-			<div className='cart-items'></div>
+			<div className='cart-items'>
+				{cartItems.map((item) => (
+					<CartItem key={item.id} item={item} />
+				))}
+			</div>
 			<CustomButton> go to check out</CustomButton>
 		</div>
 	);
