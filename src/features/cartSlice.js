e6920c7605bacbbb5070d1dea9329d05db165a28
Park cloud <340688748@qq.com>
2021-02-22 15:38:49 +0800

    g

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -11,14 +11,32 @@ export const cartSlice = createSlice({
 		toggleCartDropDown: (state, action) => {
 			state.IsOpen = !state.IsOpen;
 		},
-		addItem: (state, action) => {
-			const { id } = action.payload;
-			const existingCartItem = state.cartItems.find((item) => item.id === id);
-			if (existingCartItem) {
-				existingCartItem.quantity === undefined
-					? (existingCartItem.quantity = 1)
-					: (existingCartItem.quantity += 1);
-			} else state.cartItems.push(action.payload);
+		// addItem: (state, action) => {
+		// 	const { id } = action.payload;
+		// 	const payload = action;
+		// 	const existingCartItem = state.cartItems.find((item) => item.id === id);
+		// 	if (existingCartItem) {
+		// 		existingCartItem.quantity === undefined
+		// 			? (existingCartItem.quantity = 2)
+		// 			: (existingCartItem.quantity += 1);
+		// 	} else state.cartItems.push(payload);
+		// },
+		addItem: {
+			reducer(state, action) {
+				const { id } = action.payload;
+				const existingCartItem = state.cartItems.find((item) => item.id === id);
+				if (existingCartItem) {
+					existingCartItem.quantity += 1;
+				} else {
+					state.cartItems.push(action.payload);
+				}
+			},
+			prepare(item) {
+				const quantity = 1;
+				return {
+					payload: { ...item, quantity },
+				};
+			},
 		},
 	},
 });
