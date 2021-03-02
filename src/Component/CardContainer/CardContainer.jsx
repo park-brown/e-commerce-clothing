cdb2cb3f55b46c92422bfabb0999a59299977b74
Park cloud <340688748@qq.com>
2021-03-02 15:10:47 +0800

    make it better

--- src/Component/CardContainer/CardContainer.jsx
+++ src/Component/CardContainer/CardContainer.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import './CardContainer.styles.scss';
 import { Card } from '../Card/Card';
 export const CardContainer = ({ title, items }) => {
+	const theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.down('xs'));
 	return (
 		<div className='collection-preview '>
 			<h1 className='title'>{title}</h1>
 			<div className='preview'>
 				{items
-					.filter((item, index) => index < 4)
+					.filter((item, index) => (matches ? index < 1 : index < 4))
 					.map((item) => (
 						<Card item={item} key={item.id} />
 					))}
